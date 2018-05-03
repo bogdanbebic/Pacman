0a4e4dafd961f3e2306f40c98594ad9ea2dfbe66
bogdanbebic <bb170011d@student.etf.bg.ac.rs>
2018-05-03 18:47:52 +0200

    Dodate 8 bit pesme u folder Music, stavio da mogu da se pustaju iz main.c

--- Pacman/Pacman/main.c
+++ Pacman/Pacman/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+#include <Windows.h>
+
+#pragma comment(lib, "winmm.lib")
+
 int main() {
 	printf("Hello, World!\n");
+	PlaySound(TEXT("Music/ImperialMarch"), NULL, SND_SYNC);
 	return 0;
 }
