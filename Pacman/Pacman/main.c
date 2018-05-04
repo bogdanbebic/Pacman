1e6f7fee076cd0c02ad307c344ed85c4af9efc62
bogdanbebic <bb170011d@student.etf.bg.ac.rs>
2018-05-04 15:23:29 +0200

    Setup-ovan SDL

--- Pacman/Pacman/main.c
+++ Pacman/Pacman/main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
 #include <Windows.h>
-
 #pragma comment(lib, "winmm.lib")
 
-int main() {
+#include <SDL.h>
+
+
+int main(int argc, char *argv[]) {
+
+
 	printf("Hello, World!\n");
 	PlaySound(TEXT("Music/ImperialMarch"), NULL, SND_SYNC);
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		printf("SDL_Init failed: %s\n", SDL_GetError());
+		return 0;
+	}
+
+
+
 	return 0;
 }
